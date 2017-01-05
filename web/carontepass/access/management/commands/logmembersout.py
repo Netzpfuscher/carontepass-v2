46bee6760b819fb07b312e342535f596dab8723b
gingebot <gingebot@gmail.com>
2017-01-05 19:05:57 +0000

    logmembersout command added

---
+++ web/carontepass/access/management/commands/logmembersout.py
@@ -0,0 +1,11 @@
+from access.models import Log
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+
+class Command(BaseCommand):
+
+    help = 'Logs all currently logged in users out.'
+
+    def handle(self, *args, **options):
+        Log.objects.filter(user_in=True).update(user_in=False, ts_output=timezone.now())
